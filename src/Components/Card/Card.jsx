e24ae34016d9b15707c6551f0443be54bc171a3b
AlanSilcher <silcheralan@gmail.com>
2023-05-16 15:28:00 -0300

    Imagen

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
     return(
         <Link to={`/detail/${props.id}`} className={styles.link}>
         <div key = {props.id} className={styles.card}>
-           <img className={styles.image} src={props.image}/>
+           <img className={styles.image} src={props.image} alt="food"/>
             <p className={styles.title}>{props.name}</p>
             <div className={styles.diets}>
            <p className={styles.listItem}>{Array.isArray(props.diets)? props.diets.join(', '): "It does not belong to any diet"}</p>
